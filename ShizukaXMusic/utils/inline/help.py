431142ab44c9411db937b340e905f50a1a66b9b8
NkBots <116135526+NkBots@users.noreply.github.com>
2023-03-02 13:01:43 +0530

    Update help.py

--- ShizukaXMusic/utils/inline/help.py
+++ ShizukaXMusic/utils/inline/help.py
@@ -4,27 +4,32 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from ShizukaXMusic import app
 
-
 def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close")]
+    first = [
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        )
+    ]
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            callback_data="help_back",
+            callback_data=f"settingsback_helper",
         ),
-        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"),
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        ),
     ]
     mark = second if START else first
     upl = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
                     text=_["H_B_1"],
                     callback_data="help_callback hb1",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_2"],
+                    callback_data="help_callback hb2",
                 ),
             ],
             [
@@ -38,10 +43,6 @@ def help_pannel(_, START: Union[bool, int] = None):
                 ),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
                 InlineKeyboardButton(
                     text=_["H_B_6"],
                     callback_data="help_callback hb5",
@@ -61,7 +62,9 @@ def help_back_markup(_):
                     text=_["BACK_BUTTON"],
                     callback_data=f"settings_back_helper",
                 ),
-                InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"], callback_data=f"close"
+                ),
             ]
         ]
     )
