8455dd24eef679da2d8df3b039a0d16cd205fead
NkBots <116135526+NkBots@users.noreply.github.com>
2023-03-02 12:05:56 +0530

    Update inline.py

--- ShizukaXMusic/plugins/bot/inline.py
+++ ShizukaXMusic/plugins/bot/inline.py
@@ -42,16 +42,17 @@ async def inline_query_handler(client, query):
                     ],
                 ]
             )
+           
             searched_text = f"""
-📌**ᴛɪᴛʟᴇ:** [{title}]({link})
+❇️**Title:** [{title}]({link})
+⏳**Duration:** {duration} Mins
+👀**Views:** `{views}`
+⏰**Published Time:** {published}
+🎥**Channel Name:** {channel}
+📎**Channel Link:** [Visit From Here]({channellink})
+__Reply with /play on this searched message to stream it on voice chat.__
+⚡️ ** Inline Search By {MUSIC_BOT_NAME} **"""
 
-⏳**ᴅᴜʀᴀᴛɪᴏɴ:** {duration} Mins
-👀**ᴠɪᴇᴡs:** `{views}`
-⏰**ᴩᴜʙʟɪsʜᴇᴅ ᴏɴ:** {published}
-🎥**ᴄʜᴀɴɴᴇʟ:** {channel}
-📎**ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ:** [ᴠɪsɪᴛ ᴄʜᴀɴɴᴇʟ]({channellink})
-
-💖 ** sᴇᴀʀᴄʜ ᴩᴏᴡᴇʀᴇᴅ ʙʏ {MUSIC_BOT_NAME} **"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
